e444becf14b566df2f7d332da2b275853b571b19
Hasan <ha03330224926@gmail.com>
2023-04-13 08:14:22 +0500

    reverse the process of cors

--- server.js
+++ server.js
@@ -45,8 +45,7 @@ const server = app.listen(PORT, console.log(`Server Successfully Connected On ${
 const io = require("socket.io")(server, {
     pingTimeout : 60000,  // a user didn't send any message or something so its gonna close the connection in 60 sec
     cors : {  // Cross Origin Error
-        // origin : "http://localhost:3000",
-        origin : "*",
+        origin : "http://localhost:3000",
     }
 })
 
